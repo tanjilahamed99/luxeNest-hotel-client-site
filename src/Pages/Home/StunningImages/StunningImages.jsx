a3243a0851e6dcfea63cd8e5b0fb781aa79a13ec
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-12 19:11:25 +0600

    new image added

--- src/Pages/Home/StunningImages/StunningImages.jsx
+++ src/Pages/Home/StunningImages/StunningImages.jsx
@@ -2,13 +2,13 @@ import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 
 const StunningImages = () => {
-    const [sliderRef] = useKeenSlider(
-        {
-            loop: true,
+                const [sliderRef] = useKeenSlider(
+                {
+                    loop: true,
         },
-        [
+                [
             (slider) => {
-                let timeout
+                    let timeout
                 let mouseOver = false
                 function clearNextTimeout() {
                     clearTimeout(timeout)
@@ -17,8 +17,8 @@ const StunningImages = () => {
                     clearTimeout(timeout)
                     if (mouseOver) return
                     timeout = setTimeout(() => {
-                        slider.next()
-                    }, 2000)
+                    slider.next()
+                }, 2000)
                 }
                 slider.on("created", () => {
                     slider.container.addEventListener("mouseover", () => {
@@ -26,51 +26,51 @@ const StunningImages = () => {
                         clearNextTimeout()
                     })
                     slider.container.addEventListener("mouseout", () => {
-                        mouseOver = false
+                    mouseOver = false
                         nextTimeout()
                     })
-                    nextTimeout()
+                nextTimeout()
                 })
                 slider.on("dragStarted", clearNextTimeout)
                 slider.on("animationEnded", nextTimeout)
                 slider.on("updated", nextTimeout)
             },
-        ]
-    )
-    return (
-        <>
-            <div ref={sliderRef} className="keen-slider">
-                <div className="keen-slider__slide bg-cover bg-center number-slide1 w-full h-[100vh] image-full"
-                    style={{ backgroundImage: 'url(https://i.ibb.co/bFKdgVW/manuel-moreno-DGa0-LQ0y-DPc-unsplash.jpg)' }}
-                >
-                    <div className="w-full">
-                        <img className="w-full mx-auto   image-full" src="" alt="" />
+                ]
+                )
+                return (
+                <>
+                    <div ref={sliderRef} className="keen-slider">
+                        <div className="keen-slider__slide bg-cover bg-center number-slide1 w-full h-[100vh] image-full"
+                            style={{ backgroundImage: 'url(https://i.ibb.co/m5ZRfGc/istockphoto-1334117334-612x612.jpg)' }}
+                        >
+                            <div className="w-full">
+                                <img className="w-full mx-auto   image-full" src="" alt="" />
+                            </div>
+                        </div>
+                        <div className="keen-slider__slide bg-cover bg-center number-slide2 w-full h-[100vh] image-full"
+                            style={{ backgroundImage: 'url(https://i.ibb.co/vsVWB7w/miami-florida-hotel-room-wallpaper-thumb.jpg)' }}
+                        >
+                            <div className="w-full">
+                                <img className="w-full mx-auto   image-full" src="" alt="" />
+                            </div>
+                        </div>
+                        <div className="keen-slider__slide bg-cover bg-center number-slide3 w-full h-[100vh] image-full"
+                            style={{ backgroundImage: 'url(https://i.ibb.co/SQ49gWm/f516ccf41e60b135c1fd82aa96803326.jpg)' }}
+                        >
+                            <div className="w-full">
+                                <img className="w-full mx-auto   image-full" src="" alt="" />
+                            </div>
+                        </div>
+                        <div className="keen-slider__slide bg-cover bg-center number-slide4 w-full h-[100vh] image-full"
+                            style={{ backgroundImage: 'url(https://i.ibb.co/8gyzsj8/istockphoto-1452529483-612x612.jpg)' }}
+                        >
+                            <div className="w-full">
+                                <img className="w-full mx-auto   image-full" src="" alt="" />
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="keen-slider__slide bg-cover bg-center number-slide2 w-full h-[100vh] image-full"
-                    style={{ backgroundImage: 'url(https://i.ibb.co/KVtLrzj/valeriia-bugaiova-p-PHge-Hz1uk-unsplash.jpg)' }}
-                >
-                    <div className="w-full">
-                        <img className="w-full mx-auto   image-full" src="" alt="" />
-                    </div>
-                </div>
-                <div className="keen-slider__slide bg-cover bg-center number-slide3 w-full h-[100vh] image-full"
-                    style={{ backgroundImage: 'url(https://i.ibb.co/K9pCCy3/christian-lambert-vm-IWr0-Nnp-CQ-unsplash.jpg)' }}
-                >
-                    <div className="w-full">
-                        <img className="w-full mx-auto   image-full" src="" alt="" />
-                    </div>
-                </div>
-                <div className="keen-slider__slide bg-cover bg-center number-slide4 w-full h-[100vh] image-full"
-                    style={{ backgroundImage: 'url(https://i.ibb.co/BcRK3gy/johannes-w-IQKu-Hc2lils-unsplash.jpg)' }}
-                >
-                    <div className="w-full">
-                        <img className="w-full mx-auto   image-full" src="" alt="" />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+                </>
+                )
 };
 
-export default StunningImages;
+                export default StunningImages;
